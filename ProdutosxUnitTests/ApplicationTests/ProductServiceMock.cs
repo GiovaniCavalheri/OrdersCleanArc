92acbe6a1de0178c536b19b46fd8644127737e6f
Giovani Cavalheri <inocenciogiovanin@gmail.com>
2026-09-01 14:40:53 -0300

    Finalizando testes unitários

--- ProdutosxUnitTests/ApplicationTests/ProductServiceMock.cs
+++ ProdutosxUnitTests/ApplicationTests/ProductServiceMock.cs
@@ -45,4 +45,79 @@ public class ProductServiceTests
 
         await act.Should().ThrowAsync<KeyNotFoundException>();
     }
+
+    [Fact]
+    public async Task ListProductAsync_DadosValidos_DeveRetornarListaDeProdutos()
+    {
+        var mockRepo = new Mock<IProductRepository>();
+        var products = new List<Product>
+        {
+            new Product("Mouse", 100m, 10),
+            new Product("Teclado", 150m, 5),
+            new Product("Monitor", 800m, 3)
+        };
+        mockRepo.Setup(repo => repo.ListAllAsync()).ReturnsAsync(products);
+        var productService = new ProductService(mockRepo.Object);
+        var result = await productService.ListProductsAsync();
+        result.Should().BeEquivalentTo(products.Select(p => new ProductResponseDTO
+        {
+            Id = p.Id,
+            ProductName = p.ProductName,
+            ProductPrice = p.ProductPrice,
+            QuantityStock = p.QuantityStock
+        }));
+    }
+
+    [Fact]
+    public async Task UpdateProductAsync_ProdutoExiste_DeveAtualizarProdutoComSucesso()
+    {
+        var existingProduct = new Product("Mouse Gamer", 100m, 10);
+        var mockRepo = new Mock<IProductRepository>();
+        mockRepo.Setup(repo => repo.GetByIdAsync(existingProduct.Id)).ReturnsAsync(existingProduct);
+        mockRepo.Setup(repo => repo.UpdateAsync(It.IsAny<Product>())).Returns(Task.CompletedTask);
+        var dto = new ProductResponseDTO
+        {
+            ProductName = "Mouse Gamer",
+            ProductPrice = 1200m,
+            QuantityStock = 10
+        };
+        var productService = new ProductService(mockRepo.Object);
+        var result = await productService.UpdateProductPriceAsync (existingProduct.Id, dto.ProductPrice);
+        result.ProductName.Should().Be(dto.ProductName);
+        result.ProductPrice.Should().Be(dto.ProductPrice);
+        result.QuantityStock.Should().Be(dto.QuantityStock);
+    }
+
+    [Fact]
+    public async Task UpdateProductStockAsync_ProdutoExiste_DeveAtualizarStock()
+    {
+        var product = new Product("Mouse Gamer", 100m, 5);
+        var mockRepo = new Mock<IProductRepository>();
+        mockRepo.Setup(repo => repo.GetByIdAsync(product.Id)).ReturnsAsync(product);
+        mockRepo.Setup(repo => repo.UpdateAsync(It.IsAny<Product>())).Returns(Task.CompletedTask);
+        var dto1 = new ProductResponseDTO
+        {
+            ProductName = "Mouse Gamer",
+            ProductPrice = 100m,
+            QuantityStock = 10
+        };
+        var productServ = new ProductService(mockRepo.Object);
+        var result = await productServ.UpdateProductStockAsync(product.Id, dto1.QuantityStock);
+        result.ProductName.Should().Be(dto1.ProductName);
+        result.ProductPrice.Should().Be(dto1.ProductPrice);
+        result.QuantityStock.Should().Be(dto1.QuantityStock);
+    }
+
+    [Fact]
+    public async Task DeleteProductAsync_ProdutoExiste_DeveDeletarComSucesso()
+    {
+        var existingProduct = new Product("Mouse Gamer", 100m, 10);
+        var mockRepo = new Mock<IProductRepository>();
+        mockRepo.Setup(repo => repo.GetByIdAsync(existingProduct.Id)).ReturnsAsync(existingProduct);
+        mockRepo.Setup(repo => repo.DeleteAsync(existingProduct.Id)).Returns(Task.CompletedTask);
+        var productService = new ProductService(mockRepo.Object);
+        Func<Task> act = async () => await productService.DeleteProductAsync(existingProduct.Id);
+        await act.Should().NotThrowAsync();
+    }
+
 }
