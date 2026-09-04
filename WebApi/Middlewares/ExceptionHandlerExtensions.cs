3228c59f590c2e4d8c62815932cbff0685a70e6a
Giovani Cavalheri <inocenciogiovanin@gmail.com>
2026-09-04 14:41:12 -0300

    Extensão Environment verificando o ambiente de dev ou prod

--- WebApi/Middlewares/ExceptionHandlerExtensions.cs
+++ WebApi/Middlewares/ExceptionHandlerExtensions.cs
@@ -4,30 +4,37 @@ using Microsoft.AspNetCore.Diagnostics;
 
 public static class ExceptionHandlerExtensions
 {
-    public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+    public static void ConfigureExceptionHandler(this IApplicationBuilder app, IHostEnvironment environment)
     {
         app.UseExceptionHandler(appError =>
         {
             appError.Run(async context =>
             {
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
 
-                context.Response.StatusCode = StatusCodes.Status500InternalServerError; 
-                context.Response.ContentType = "application/json";
-
-                var contextFeature = context.Features.Get<Microsoft.AspNetCore.Diagnostics.IExceptionHandlerFeature>();
                 if (contextFeature != null)
                 {
+                    var statusCode = contextFeature.Error switch
+                    {
+                        ArgumentException => StatusCodes.Status400BadRequest,
+                        KeyNotFoundException => StatusCodes.Status404NotFound,
+                        _ => StatusCodes.Status500InternalServerError
+                    };
+
+                    context.Response.StatusCode = statusCode;
+                    context.Response.ContentType = "application/json";
+
                     await context.Response.WriteAsync(new ErrorDetails()
                     {
-                        StatusCode = contextFeature.Error switch
-                        {
-                            ArgumentException => StatusCodes.Status400BadRequest,
-                            KeyNotFoundException => StatusCodes.Status404NotFound,
-                            _ => StatusCodes.Status500InternalServerError
-                        },
+                        StatusCode = statusCode,
                         Message = contextFeature.Error.Message,
                         Trace = contextFeature.Error.StackTrace
                     }.ToString());
+                }
+                else
+                {
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Response.ContentType = "application/json";
                 }
             });
         });
